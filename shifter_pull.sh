e1b95e2976528498d34da8964cf34bb77ab22676
Matthew Feickert <matthew.feickert@cern.ch>
2021-08-03 01:21:07 -0500

    Add shifter script

---
+++ shifter_pull.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+module load shifter
+# pyhf
+shifterimg pull neubauergroup/bluewaters-pyhf:0.6.2
+# MadGraph5
+shifterimg pull neubauergroup/bluewaters-mg5_amc:3.1.1
+shifterimg pull neubauergroup/bluewaters-mg5_amc:latest
+# MoMEMta
+shifterimg pull neubauergroup/bluewaters-momemta:1.0.1
+shifterimg pull neubauergroup/bluewaters-momemta:latest
+# Delphes
+shifterimg pull scailfin/delphes-python-centos:3.5.0
+shifterimg pull scailfin/delphes-python-centos:latest
